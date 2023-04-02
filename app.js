575e6f347d4d0158f888d5283ed289cc45a1ae7f
Jigar Sutariya <jigarsutariya2608@gmail.com>
2023-04-02 08:33:34 +0530

    CHANGED ORIGIN

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("./app/mongoose");
 const routes = require("./app/routes");
 const cors = require("cors");
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+// http://localhost:3000
+app.use(cors({ origin: "https://moonlit-pithivier-da5ced.netlify.app" }));
 
 mongoose.configure();
 
